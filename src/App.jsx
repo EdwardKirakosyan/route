c3ecfca88b7ec395a7fdf7e36120805812ba2330
Edward Kirakosyan <kirakosyan3010@gmail.com>
2023-11-13 15:44:59 +0400

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
+import { Link, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import BookList from "./pages/BookList"
+import Book from "./pages/Book"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+      </Routes>
+    </>
   )
 }
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 export default function Book() {
   return <h1>book</h1>
 }
